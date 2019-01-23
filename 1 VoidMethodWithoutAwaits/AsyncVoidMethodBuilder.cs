c62d2b08c29c43f98fb5271291b0432c861ca094
Siyuan Zheng <siyzhe@microsoft.com>
2019-01-23 16:24:19 +0800

    add VoidMethodWithoutAwaits

---
+++ 1 VoidMethodWithoutAwaits/AsyncVoidMethodBuilder.cs
@@ -0,0 +1,44 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    interface INotifyCompletion { }
+    interface ICriticalNotifyCompletion { }
+
+    public interface IAsyncStateMachine
+    {
+        void MoveNext();
+        void SetStateMachine(IAsyncStateMachine stateMachine);
+    }
+
+    public struct AsyncVoidMethodBuilder
+    {
+        public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+        {
+        }
+        public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+        {
+        }
+        public static AsyncVoidMethodBuilder Create()
+        {
+            return new AsyncVoidMethodBuilder();
+        }
+        public void SetException(Exception e)
+        {
+            Console.WriteLine("SetException called");
+        }
+
+        public void SetResult()
+        {
+            Console.WriteLine("SetResult called");
+        }
+        public void SetStateMachine(IAsyncStateMachine stateMachine)
+        {
+            Console.WriteLine("SetStateMachine called");
+
+        }
+        public void Start<TStateMachine>(ref TStateMachine stateMachine)
+        // where TStateMachine : IAsyncStateMachine
+        {
+
+        }
+    }
+}
---
+++ 1 VoidMethodWithoutAwaits/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Eduasync
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DoNothingAsync();
+        }
+
+        // Warning CS1998 is about a method with no awaits in... exactly what we're trying to
+        // achieve!
+#pragma warning disable 1998
+        private static async void DoNothingAsync()
+        {
+        }
+#pragma warning restore 1998
+    }
+}
