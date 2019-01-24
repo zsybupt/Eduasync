6704ed7febf385452277130d8149adb3f862ceca
zsybupt <zsybupt@outlook.com>
2019-01-24 10:37:07 +0800

    add simple AsyncTaskMethodBuilder

---
+++ 3 FirstTaskCompletionSource/AsyncTaskMethodBuilder.cs
@@ -0,0 +1,65 @@
+﻿
+using System.Threading.Tasks;
+
+namespace System.Runtime.CompilerServices
+{
+    public interface INotifyCompletion
+    {
+        void OnCompleted(Action continuation);
+    }
+    public interface ICriticalNotifyCompletion
+    {
+        void UnsafeOnCompleted(Action continuation);
+    }
+
+    public interface IAsyncStateMachine
+    {
+        void MoveNext();
+        void SetStateMachine(IAsyncStateMachine stateMachine);
+    }
+
+    public struct AsyncTaskMethodBuilder<T>
+    {
+        private TaskCompletionSource<T> tcs;
+
+        public AsyncTaskMethodBuilder(TaskCompletionSource<T> tcs)
+        {
+            this.tcs = tcs;
+        }
+        public Task<T> Task => this.tcs.Task;
+
+        public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine) where TAwaiter: INotifyCompletion where TStateMachine : IAsyncStateMachine
+        {
+            awaiter.OnCompleted(stateMachine.MoveNext);
+        }
+        public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine) where TAwaiter : ICriticalNotifyCompletion where TStateMachine : IAsyncStateMachine
+        {
+            awaiter.UnsafeOnCompleted(stateMachine.MoveNext);
+        }
+        public static AsyncTaskMethodBuilder<T> Create()
+        {
+            return new AsyncTaskMethodBuilder<T>(new TaskCompletionSource<T>());
+        }
+        public void SetException(Exception e)
+        {
+            Console.WriteLine("SetException called");
+        }
+
+        public void SetResult(T result)
+        {
+            this.tcs.SetResult(result);
+            Console.WriteLine("SetResult called");
+        }
+        public void SetStateMachine(IAsyncStateMachine stateMachine)
+        {
+            Console.WriteLine("SetStateMachine called");
+
+        }
+        public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine
+        {
+            Console.WriteLine("Start called");
+
+            stateMachine.MoveNext();
+        }
+    }
+}
---
+++ 3 FirstTaskCompletionSource/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Eduasync
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Task<int> task = Return10Async();
+            Console.WriteLine("Main got result: {0}", task.Result);
+            Console.ReadKey();
+        }
+
+        // Warning CS1998 is about a method with no awaits in... exactly what we're trying to
+        // achieve!
+#pragma warning disable 1998
+        private static async Task<int> Return10Async()
+        {
+            return 10;
+        }
+#pragma warning restore 1998
+    }
+}
